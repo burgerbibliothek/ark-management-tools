3c40ca1dc687dbb68816bb18d29c55f0bc617f5a
Giò Diani <mail@gionathandiani.name>
2024-12-05 22:48:33 +0100

    Updated NCDA tests.

--- tests/NcdaTest.php
+++ tests/NcdaTest.php
@@ -5,6 +5,9 @@ use PHPUnit\Framework\TestCase;
 
 class NcdaTest extends TestCase
 {
+    
+    public $checkZone = '13030/xf93gt2';
+    public $xdigits = '0123456789bcdfghjkmnpqrstvwxz000';
     /**
      * Test NOID CHECK DIGIT ALGORITHM.
      * https://metacpan.org/dist/Noid/view/noid#NOID-CHECK-DIGIT-ALGORITHM
@@ -12,18 +15,22 @@ class NcdaTest extends TestCase
     public function test_noid_check_digit_algortihm_works(): void
     {
         
-        $checkZone = '13030/xf93gt2';
-        $repetoire = 'rp4444kj0f91zbgntw3qsdcm822227v6h5x';
-        $illegalRepetoire = '123abc@#_-';
+        $checkdigit = Ncda::calc($this->checkZone, $this->xdigits);
+        $this->assertSame('q', $checkdigit, 'NCDA failed.');
 
-        $checkdigit = Ncda::calc($checkZone, $repetoire);
-        $this->assertSame('q', $checkdigit);
+        $this->assertTrue(Ncda::verify($this->checkZone.$checkdigit, $this->xdigits), 'Failed to verify that the NCDA is true.');
 
-        $verifyNoid = Ncda::verify($checkZone.$checkdigit, $repetoire);
-        $this->assertTrue($verifyNoid);
+    }
 
-        $checkIllegealRepetoire = Ncda::calc($checkZone, $illegalRepetoire);
-        $this->assertFalse($checkIllegealRepetoire);
-        
+    public function test_noid_check_digit_algortihm_length_exception(): void
+    {
+        $this->expectException(Exception::class);
+        Ncda::calc($this->checkZone.'-._$@', $this->xdigits);
+    }
+
+    public function test_noid_check_digit_algortihm_well_formedness(): void
+    {
+        $this->expectException(Exception::class);
+        Ncda::calc($this->checkZone.'-._$@', $this->xdigits);
     }
 }
